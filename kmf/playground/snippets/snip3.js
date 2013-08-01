e9bc6ae048f18697370b8bb5e3c99c37e9d469a5
Francois Fouquet <fouquet.f@gmail.com>
2013-08-01 11:29:57 +0200

    cleanup

--- kmf/playground/snippets/snip3.js
+++ kmf/playground/snippets/snip3.js
@@ -1,1 +1,31 @@
-//TODO
+var cloud = factory.createCloud();
+var myAmazonEC2node = factory.createNode();
+myAmazonEC2node.setId("EC2_0");
+cloud.addNodes(myAmazonEC2node);
+var myNginx = factory.createSoftware();
+myNginx.setName("SRV0");
+myAmazonEC2node.addSoftwares(myNginx);
+
+var emptyCloud = factory.createCloud();
+
+//Produce the trace sequence of MO -> M1
+var diffSeq = compare.diff(emptyCloud,cloud);
+oldConsole.log(diffSeq);
+console.log(diffSeq.exportToString());
+
+//Produce the trace sequence of MO inter M1
+var interSeq = compare.inter(emptyCloud,cloud);
+
+
+for(i=0;i < diffSeq.getTraces().$size;i++){
+	//Print the comparison between M0 and M1
+	console.log(diffSeq.getTraces()[i]);
+}
+//Merge M1 into M0
+new ModelTraceApplicator(model0)
+   .applyTraceOnModel(traces);
+assert(model1.modelEquals(m0));
+var model3 = factory.createCloud();
+new ModelTraceApplicator(model3)
+   .applyTraceOnModel(tracesInter);
+//model3 is now the common part of M0 and M1
--- kmf/playground/snippets/snip4.js
+++ kmf/playground/snippets/snip4.js
@@ -1,1 +1,28 @@
-//TODO
+var cloud = factory.createCloud();
+var myAmazonEC2node = factory.createNode();
+myAmazonEC2node.setId("EC2_0");
+cloud.addNodes(myAmazonEC2node);
+var myNginx = factory.createSoftware();
+myNginx.setName("SRV0");
+myAmazonEC2node.addSoftwares(myNginx);
+
+var listener = { elementChanged : function(event){
+ switch(event.getType()){
+  case ActionType.$ADD:
+    console.log("Add event ! "+event);
+	break;
+  case ActionType.$SET:
+    console.log("Set event ! "+event);
+	break;
+ }
+}};
+//Add as Tree (meaning global) listener
+cloud.addModelTreeListener(listener);
+//Std listener only listen local event
+//cloud.addModelListener(listener);
+var node = factory.createNode();
+node.setId("EC1");
+//Trigger event ADD 
+cloud.addNodes(node);
+//Trigger event SET
+node.setId("EC2_1");
