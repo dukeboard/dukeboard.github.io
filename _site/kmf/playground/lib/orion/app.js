dc7401f2070c83940db18477fb7152c73fff4938
Francois Fouquet <fouquet.f@gmail.com>
2013-08-09 10:20:43 +0200

    add orion experiment
    migrate to new optimized version of KMF JS for TinyCloud sample

---
+++ _site/kmf/playground/lib/orion/app.js
@@ -0,0 +1,9 @@
+include("org.kevoree.modeling.sample.cloud.js.js");
+include("kotlin-lib-ecma3.js");
+include("kotlin-lib.js");
+include("kotlin-map.js");
+
+<script src="../kotlin-lib-ecma3.js"></script>
+<script src="../kotlin-lib.js"></script>
+<script src="../kotlin-maps.js"></script>
+<script type="text/javascript" src="../org.kevoree.modeling.sample.cloud.js.js"></script>
