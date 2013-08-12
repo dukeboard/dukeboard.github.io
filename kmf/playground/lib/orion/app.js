4483b40e4dcb9437e9b2af5e0ff6d3087073ac0b
Francois Fouquet <fouquet.f@gmail.com>
2013-08-13 00:40:19 +0200

    new playground

--- kmf/playground/lib/orion/app.js
+++
@@ -1,9 +0,0 @@
-include("org.kevoree.modeling.sample.cloud.js.js");
-include("kotlin-lib-ecma3.js");
-include("kotlin-lib.js");
-include("kotlin-map.js");
-
-<script src="../kotlin-lib-ecma3.js"></script>
-<script src="../kotlin-lib.js"></script>
-<script src="../kotlin-maps.js"></script>
-<script type="text/javascript" src="../org.kevoree.modeling.sample.cloud.js.js"></script>
